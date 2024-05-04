41a3500ac9e56d4bd975381e7bb11da15ba07581
Mroik <mroik@delayed.space>
2024-05-04 02:15:27 +0200

    Add AND intro-elim

--- src/fitch.rs
+++ src/fitch.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, rc::Rc};
+use std::{borrow::Borrow, fmt::Display, rc::Rc};
 
 enum Proposition {
     Absurdum,
@@ -20,6 +20,20 @@ impl Display for Proposition {
             Self::Not(t) => write!(f, "(!{})", t),
             Self::Implies(left, right) => write!(f, "({} => {})", left, right),
             Self::Iff(left, right) => write!(f, "({} <=> {})", left, right),
+        }
+    }
+}
+
+impl PartialEq for Proposition {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Term(l0), Self::Term(r0)) => l0 == r0,
+            (Self::And(l0, l1), Self::And(r0, r1)) => l0 == r0 && l1 == r1,
+            (Self::Or(l0, l1), Self::Or(r0, r1)) => l0 == r0 && l1 == r1,
+            (Self::Not(l0), Self::Not(r0)) => l0 == r0,
+            (Self::Implies(l0, l1), Self::Implies(r0, r1)) => l0 == r0 && l1 == r1,
+            (Self::Iff(l0, l1), Self::Iff(r0, r1)) => l0 == r0 && l1 == r1,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
         }
     }
 }
@@ -60,10 +74,10 @@ enum FitchComponent {
 }
 
 impl FitchComponent {
-    fn unwrap(&self) -> Rc<Proposition> {
+    fn unwrap(&self) -> &Rc<Proposition> {
         match self {
-            FitchComponent::Assumption(t) => t.clone(),
-            FitchComponent::Deduction(t) => t.clone(),
+            FitchComponent::Assumption(t) => t,
+            FitchComponent::Deduction(t) => t,
         }
     }
 }
@@ -73,6 +87,7 @@ type Level = u32;
 struct Fitch {
     statements: Vec<(Level, FitchComponent)>,
     start_of_deductions: usize,
+    current_level: u32,
 }
 
 impl Display for Fitch {
@@ -100,6 +115,7 @@ impl Fitch {
         Fitch {
             statements: Vec::new(),
             start_of_deductions: 0,
+            current_level: 0,
         }
     }
 
@@ -116,5 +132,60 @@ impl Fitch {
         if self.statements.len() < self.start_of_deductions {
             self.start_of_deductions = self.statements.len();
         }
+    }
+
+    fn introduce_and(&mut self, left: usize, right: usize) -> bool {
+        let left = match self.statements.get(left) {
+            Some(v) => v,
+            None => return false,
+        };
+        let right = match self.statements.get(right) {
+            Some(v) => v,
+            None => return false,
+        };
+
+        let ris = Proposition::new_and(left.1.unwrap(), right.1.unwrap());
+        self.statements
+            .push((self.current_level, FitchComponent::Deduction(ris)));
+        true
+    }
+
+    fn eliminate_and(&mut self, assum: usize, prop: &Rc<Proposition>) -> bool {
+        let assum = match self.statements.get(assum) {
+            Some(v) => v.1.unwrap(),
+            None => return false,
+        };
+
+        match assum.borrow() {
+            Proposition::And(left, _) if left == prop => {
+                self.statements
+                    .push((self.current_level, FitchComponent::Deduction(left.clone())));
+                true
+            }
+            Proposition::And(_, right) if right == prop => {
+                self.statements
+                    .push((self.current_level, FitchComponent::Deduction(right.clone())));
+                true
+            }
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Fitch, Proposition};
+
+    #[test]
+    fn eliminate_and() {
+        let mut fitch = Fitch::new();
+        let t0 = Proposition::new_term("A");
+        let t1 = Proposition::new_term("B");
+        let prop = Proposition::new_and(&t0, &t1);
+        fitch.add_assumption(&prop);
+        let ris = fitch.eliminate_and(0, &Proposition::new_term("A"));
+        assert!(ris);
+        let ris = fitch.eliminate_and(0, &Proposition::new_term("C"));
+        assert!(!ris);
     }
 }
